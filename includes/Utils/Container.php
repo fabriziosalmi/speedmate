d5d0d8f4d3659094474d9745141e3ac27f8e67b5
Fabrizio Salmi <fabrizio.salmi@gmail.com>
2026-01-22 20:44:06 +0100

    fix: WordPress standards for CspNonce and Container
    - Add file and class docblocks
    - Convert spaces to tabs
    - Use Yoda conditions
    - Use array() syntax
    - Add phpcs:ignore for base64_encode
    - Proper spacing

--- includes/Utils/Container.php
+++ includes/Utils/Container.php
@@ -1,21 +1,43 @@
 <?php
+/**
+ * Dependency injection container
+ *
+ * @package SpeedMate
+ */
 
 declare(strict_types=1);
 
 namespace SpeedMate\Utils;
 
-final class Container
-{
-    /** @var array<string, object> */
-    private static array $instances = [];
+/**
+ * Simple DI container for testing
+ */
+final class Container {
+	/**
+	 * Container instances
+	 *
+	 * @var array<string, object>
+	 */
+	private static array $instances = array();
 
-    public static function set(string $id, object $instance): void
-    {
-        self::$instances[$id] = $instance;
-    }
+	/**
+	 * Set instance
+	 *
+	 * @param string $id       Instance ID.
+	 * @param object $instance Instance object.
+	 * @return void
+	 */
+	public static function set( string $id, object $instance ): void {
+		self::$instances[ $id ] = $instance;
+	}
 
-    public static function get(string $id): ?object
-    {
-        return self::$instances[$id] ?? null;
-    }
+	/**
+	 * Get instance
+	 *
+	 * @param string $id Instance ID.
+	 * @return object|null
+	 */
+	public static function get( string $id ): ?object {
+		return self::$instances[ $id ] ?? null;
+	}
 }
--- includes/Utils/CspNonce.php
+++ includes/Utils/CspNonce.php
@@ -1,40 +1,65 @@
 <?php
+/**
+ * CSP Nonce utility
+ *
+ * @package SpeedMate
+ */
 
 declare(strict_types=1);
 
 namespace SpeedMate\Utils;
 
-final class CspNonce
-{
-    private static ?string $nonce = null;
+/**
+ * Content Security Policy nonce generator
+ */
+final class CspNonce {
+	/**
+	 * Nonce value
+	 *
+	 * @var string|null
+	 */
+	private static ?string $nonce = null;
 
-    public static function enabled(): bool
-    {
-        $enabled = (bool) Settings::get_value('csp_nonce', false);
+	/**
+	 * Check if CSP nonce is enabled
+	 *
+	 * @return bool
+	 */
+	public static function enabled(): bool {
+		$enabled = (bool) Settings::get_value( 'csp_nonce', false );
 
-        return (bool) apply_filters('speedmate_csp_nonce_enabled', $enabled);
-    }
+		return (bool) apply_filters( 'speedmate_csp_nonce_enabled', $enabled );
+	}
 
-    public static function get(): string
-    {
-        if (!self::enabled()) {
-            return '';
-        }
+	/**
+	 * Get nonce value
+	 *
+	 * @return string
+	 */
+	public static function get(): string {
+		if ( ! self::enabled() ) {
+			return '';
+		}
 
-        if (self::$nonce === null) {
-            self::$nonce = rtrim(strtr(base64_encode(random_bytes(16)), '+/', '-_'), '=');
-        }
+		if ( null === self::$nonce ) {
+			// phpcs:ignore WordPress.PHP.DiscouragedPHPFunctions.obfuscation_base64_encode
+			self::$nonce = rtrim( strtr( base64_encode( random_bytes( 16 ) ), '+/', '-_' ), '=' );
+		}
 
-        return self::$nonce;
-    }
+		return self::$nonce;
+	}
 
-    public static function attr(): string
-    {
-        $nonce = self::get();
-        if ($nonce === '') {
-            return '';
-        }
+	/**
+	 * Get nonce attribute
+	 *
+	 * @return string
+	 */
+	public static function attr(): string {
+		$nonce = self::get();
+		if ( '' === $nonce ) {
+			return '';
+		}
 
-        return ' nonce="' . esc_attr($nonce) . '"';
-    }
+		return ' nonce="' . esc_attr( $nonce ) . '"';
+	}
 }
